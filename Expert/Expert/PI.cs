985f67608fe42c5e30a3333af4c11ec4757da7da
NicolasLemaitre <mario.pirate@hotmail.fr>
2021-04-13 12:01:59 +0200

    Calculer PI

---
+++ Expert/Expert/PI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expert
+{
+    class PI
+    {
+        public class Point
+        {
+            public double x, y;
+        }
+        public static double Approx(Point[] pts)
+        {
+            int v = 0;
+            foreach (Point pt in pts)
+            {
+                if (((float)Math.Pow(pt.x, 2) + (float)Math.Pow(pt.y, 2)) <= 1)
+                {
+                    v++;
+                }
+            }
+            return (4f * v / (float)pts.Length);
+
+        }
+    }
+}
--- Expert/Expert/Program.cs
+++ Expert/Expert/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static Expert.PI;
 
 namespace Expert
 {
@@ -6,7 +7,45 @@ namespace Expert
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args[0] == "Billet")
+            {
+                long s = 1;
+
+                Change change = CaisseAutomatique.OptimalChange(s);
+
+                Console.WriteLine("monnaie " + s);
+
+                Console.WriteLine("Coin(s) 2€: " + change.coin2);
+                Console.WriteLine("Bill(s) 5€: " + change.bill5);
+                Console.WriteLine("Bill(s) 10€: " + change.bill10);
+
+                
+            }
+            if(args[0] == "Danse")
+            {
+                Console.WriteLine(NouvelleDanse.GetPositionAt(3)); //-4
+                Console.WriteLine(NouvelleDanse.GetPositionAt(100000)); //-5
+                Console.WriteLine(NouvelleDanse.GetPositionAt(2147483647)); //1
+              
+            }
+            if(args[0] == "PI")
+            {
+                var rands = new Point[100000];
+                Random random = new Random();
+
+                for (int i = 0; i < rands.Length; i++)
+                {
+                    Point p = new Point();
+                    p.x = random.NextDouble(); //x
+                    p.y = random.NextDouble(); //y
+                    rands[i] = p;
+                }
+
+                double Test = PI.Approx(rands);
+
+                Console.WriteLine(Test);
+            }
+            Console.ReadKey();
         }
     }
 }
