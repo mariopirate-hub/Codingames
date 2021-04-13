6fa5c78796092d269410fa1f06ab5365274c9da9
NicolasLemaitre <mario.pirate@hotmail.fr>
2021-04-13 15:24:11 +0200

    Intervalle

---
+++ Expert/Expert/Excercice/Intervalle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Expert.Excercice
+{
+    class Intervalle
+    {
+        static public int Calc(int[] array, int n1, int n2)
+        {
+            List<int> intervalle = new List<int>();
+
+            for (int i = n1; i <= n2; i++)
+
+                intervalle.Add(array[i]);
+
+
+
+            return intervalle.Sum();
+        }
+
+    }
+}
