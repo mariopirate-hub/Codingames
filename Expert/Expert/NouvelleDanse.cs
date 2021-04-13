a7c5d47f2a122a966be602066d3204b2f945af77
NicolasLemaitre <mario.pirate@hotmail.fr>
2021-04-13 11:51:30 +0200

    NouvelleDanse

---
+++ Expert/Expert/NouvelleDanse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expert
+{
+    class NouvelleDanse
+    {
+        public static int GetPositionAt(int n)
+        {
+            int[] deplacement = new int[3];
+            int[] Possition = new int[3];
+
+            List<int> valeur = new List<int>();
+
+            //étape 0
+            int danse = 0; //position
+            valeur.Add(danse);
+
+            //étape 1           
+            danse += 1;
+            deplacement[0] = danse;
+            Possition[0] = danse;
+            valeur.Add(danse);
+
+            //étape 2           
+            danse += -2;
+
+            deplacement[1] = -2;
+            Possition[1] = danse;
+            valeur.Add(danse);
+
+            n %= 6;
+
+            for (int i = 0; i < 4; i++)
+            {
+                deplacement[2] = (deplacement[1] - deplacement[0]);
+
+                danse = Possition[1] + deplacement[2];
+
+                deplacement[0] = deplacement[1];
+                deplacement[1] = deplacement[2];
+
+
+                Possition[1] = danse;
+                valeur.Add(danse);
+            }
+
+            return valeur[n];
+        }
+    }
+}
