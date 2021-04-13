3f0e3b3c7eb45fd5baf3c002ebf14a40b91285a1
NicolasLemaitre <mario.pirate@hotmail.fr>
2021-04-13 15:21:59 +0200

    Noeud terminal

---
+++ Expert/Expert/Excercice/NoeudTerminal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Expert.Excercice
+{
+    class NoeudTerminal
+    {
+        public static int FindNetworkEndpoint(int startNodeId, int[] fromIds, int[] toIds)
+        {
+            int i = Array.IndexOf(fromIds, startNodeId);
+            List<int> tt = new List<int>();
+            tt.Add(fromIds[i]);
+
+            while (!(tt.Contains(toIds[i])) && (fromIds.Contains(toIds[i])))
+            {
+
+
+                i = Array.IndexOf(fromIds, toIds[i]);
+                tt.Add(fromIds[i]);
+
+            }
+            return tt.Contains(toIds[i]) ? fromIds[i] : toIds[i];
+        }
+
+    }
+}
